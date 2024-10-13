888dd922df35aeeb26cb4a7cee8b1eeda5d83aa3
mehdi0051 <mehdiflhi5@gmail.com>
2024-10-13 08:18:34 -0700

    Add files via upload

---
+++ sin_cos/sin_cos.ino
@@ -0,0 +1,22 @@
+float amplitude = 10;
+float frequency = 0.1;
+float t = 0;
+float timeStep = 0.1;
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+  float sineValue = amplitude * sin(2 * PI * frequency * t);
+  float coseValue = amplitude * cos(2 * PI * frequency * t);
+  t += timeStep;
+  Serial.print(sineValue);
+  Serial.print("  ");
+  Serial.print(coseValue);
+  
+  
+}
